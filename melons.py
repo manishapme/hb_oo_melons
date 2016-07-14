9f439bfdba8cbcc0dd49f699bb4a4c03025f2b06
Hackbright Student <info@hackbrightacademy.com>
2016-07-14 16:33:22 -0700

    Refactored superclass and get_total.

--- melons.py
+++ melons.py
@@ -1,55 +1,51 @@
 """This file should have our order classes in it."""
 
+class AbstractMelonOrder(object):
+    """The basic structure of the melon order."""
 
-class DomesticMelonOrder(object):
-    """A domestic (in the US) melon order."""
-
-    def __init__(self, species, qty):
+    def __init__(self, species, qty, order_type, tax):
         """Initialize melon order attributes"""
 
         self.species = species
         self.qty = qty
         self.shipped = False
-        self.order_type = "domestic"
-        self.tax = 0.08
+        self.order_type = order_type
+        self.tax = tax
 
     def get_total(self):
         """Calculate price."""
-
         base_price = 5
-        total = (1 + self.tax) * self.qty * base_price
+        if self.species == "Christmas":
+            base_price = base_price * 1.5
+        if (self.order_type == "international") and (self.qty < 10):
+            additional_fee = 3
+        else:
+            additional_fee = 0
+        total = (1 + self.tax) * self.qty * base_price + additional_fee
         return total
 
     def mark_shipped(self):
         """Set shipped to true."""
 
-        self.shipped = True
+        self.shipped = True    
 
 
-class InternationalMelonOrder(object):
+class DomesticMelonOrder(AbstractMelonOrder):
+    """A domestic (in the US) melon order."""
+
+    def __init__(self, species, qty):
+        """Initialize melon order attributes"""
+        super(DomesticMelonOrder, self).__init__(species, qty, "domestic", 0.08)
+
+
+class InternationalMelonOrder(AbstractMelonOrder):
     """An international (non-US) melon order."""
 
     def __init__(self, species, qty, country_code):
         """Initialize melon order attributes"""
 
-        self.species = species
-        self.qty = qty
+        super(InternationalMelonOrder, self).__init__(species, qty, "international", 0.17)
         self.country_code = country_code
-        self.shipped = False
-        self.order_type = "international"
-        self.tax = 0.17
-
-    def get_total(self):
-        """Calculate price."""
-
-        base_price = 5
-        total = (1 + self.tax) * self.qty * base_price
-        return total
-
-    def mark_shipped(self):
-        """Set shipped to true."""
-
-        self.shipped = True
 
     def get_country_code(self):
         """Return the country code."""
